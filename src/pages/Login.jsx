0d4c5d7ed22f628a6abc2f7afe6bf68a462b13fb
KRISHNA <kraggr2909@gmail.com>
2026-01-04 18:48:57 +0530

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Login = () => {
   return (
     <div>
       <Navbar />      
-
+      <div className='min-h-screen w-full '></div>
+      <Footer />
     </div>
   )
 }
